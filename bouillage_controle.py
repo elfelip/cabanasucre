107b3c49cc9145b927654b55bb698a7d6b83de4a
elfelip <elfelip@yahoo.com>
2022-01-18 20:26:27 -0500

    Utiliser les interrupt pour la détection du niveau

--- bouillage_controle.py
+++ bouillage_controle.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
-from time import sleep
+import signal
+import sys
 import RPi.GPIO as GPIO
+
+NIV_BAS_ALERTE = 7
+NIV_BAS = 11
+NIV_MAX = 13
+NIV_MAX_ALERTE = 15
+
+def signal_handler(sig, frame):
+    GPIO.cleanup()
+    sys.exit(0)
 
 def lancer_alerte_vide():
     print("Alerte, Le chaudron est vide.")
@@ -21,36 +31,35 @@ def lancer_alerte_max():
 def lancer_erreur_niveau():
     print("Les informations de niveau sont incohérents, il doit y avoir un problème avec la sonde.")
 
-def main():
-    GPIO.setmode(GPIO.BOARD)
-    NIV_BAS_ALERTE = 7
-    NIV_BAS = 11
-    NIV_MAX = 13
-    NIV_MAX_ALERTE = 15
+def traiter_inputs_callback():
+    if ((GPIO.input(NIV_BAS_ALERTE) and not 
+        (GPIO.input(NIV_BAS) or GPIO.input(NIV_MAX) or GPIO.input(NIV_MAX_ALERTE)))):
+        lancer_alerte_bas()
+    elif ((GPIO.input(NIV_BAS_ALERTE) and GPIO.input(NIV_BAS)) and not
+        (GPIO.input(NIV_MAX) or GPIO.input(NIV_MAX_ALERTE))):
+        ouvrir_valve()
+    elif ((GPIO.input(NIV_BAS_ALERTE) and GPIO.input(NIV_BAS) and GPIO.input(NIV_MAX)) and not
+        GPIO.input(NIV_MAX_ALERTE)):
+        fermer_valve()
+    elif (GPIO.input(NIV_BAS_ALERTE) and GPIO.input(NIV_BAS) and GPIO.input(NIV_MAX)) and GPIO.input(NIV_MAX_ALERTE):
+        lancer_alerte_max()
+    elif not (GPIO.input(NIV_BAS_ALERTE) or GPIO.input(NIV_BAS) or GPIO.input(NIV_MAX) or GPIO.input(NIV_MAX_ALERTE)):
+        lancer_alerte_vide()
+    else:
+        lancer_erreur_niveau()
 
+def main():
+    GPIO.setmode(GPIO.BCM)
+    
     connecteurs = [NIV_BAS_ALERTE, NIV_BAS, NIV_MAX, NIV_MAX_ALERTE]
 
     for connecteur in connecteurs:
         GPIO.setup(connecteur, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.add_event_detect(connecteur, GPIO.RISING, callback=traiter_inputs_callback, bouncetime=200)
 
-    while 1:
-        if ((GPIO.input(NIV_BAS_ALERTE) and not 
-            (GPIO.input(NIV_BAS) or GPIO.input(NIV_MAX) or GPIO.input(NIV_MAX_ALERTE)))):
-            lancer_alerte_bas()
-        elif ((GPIO.input(NIV_BAS_ALERTE) and GPIO.input(NIV_BAS)) and not
-            (GPIO.input(NIV_MAX) or GPIO.input(NIV_MAX_ALERTE))):
-            ouvrir_valve()
-        elif ((GPIO.input(NIV_BAS_ALERTE) and GPIO.input(NIV_BAS) and GPIO.input(NIV_MAX)) and not
-            GPIO.input(NIV_MAX_ALERTE)):
-            fermer_valve()
-        elif (GPIO.input(NIV_BAS_ALERTE) and GPIO.input(NIV_BAS) and GPIO.input(NIV_MAX)) and GPIO.input(NIV_MAX_ALERTE):
-            lancer_alerte_max()
-        elif not (GPIO.input(NIV_BAS_ALERTE) or GPIO.input(NIV_BAS) or GPIO.input(NIV_MAX) or GPIO.input(NIV_MAX_ALERTE)):
-            lancer_alerte_vide()
-        else:
-            lancer_erreur_niveau()
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.pause()
             
-        sleep(1)
 
 if __name__ == "__main__":
     main()
