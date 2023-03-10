ddd6539268de1810b43ea8699aaa6f023c86875b
Philippe Gauthier <elfelip@yahoo.com>
2023-03-10 09:13:22 -0500

    Configurer consommateur OFFSET_END pour la console

--- bouillage_ctrl_cmd/console_sucrier.py
+++ bouillage_ctrl_cmd/console_sucrier.py
@@ -9,12 +9,13 @@ import logging
 import threading
 import os
 from inspqcommun.kafka.consommateur import obtenirConfigurationsConsommateurDepuisVariablesEnvironnement, creerConsommateur, decode_from_bytes
-
+from confluent_kafka import OFFSET_END, Consumer
 class ConsoleSucrier:
     topic_niveau = "bouillage.niveau"
     topic_alerte = "bouillage.alertes"
     topic_temp = "bouillage.temperature"
     logger = None
+    consommateur = None
 
     def __init__(self):
         format = "%(asctime)s: %(message)s"
@@ -34,9 +35,17 @@ class ConsoleSucrier:
         GPIO.setmode(GPIO.BCM)
         self.kafka_config = obtenirConfigurationsConsommateurDepuisVariablesEnvironnement(logger=self.logger) if 'BOOTSTRAP_SERVERS' in os.environ else None
         if self.kafka_config is not None:
-            self.kafka_config['auto_offset_reset'] = 'latest'
-        liste_topics = [self.topic_alerte, self.topic_niveau, self.topic_temp]
-        self.consommateur = creerConsommateur(config=self.kafka_config.kafka, topics=liste_topics) if self.kafka_config is not None else None
+            self.kafka_config.kafka['auto.offset.reset'] = OFFSET_END
+            liste_topics = [self.topic_alerte, self.topic_niveau, self.topic_temp]
+            #self.consommateur = creerConsommateur(config=self.kafka_config.kafka, topics=liste_topics) if self.kafka_config is not None else None
+            self.consommateur = Consumer(self.kafka_config.kafka)
+            self.consommateur.subscribe(liste_topics, on_assign=self.reset_offset)
+
+    # Set up a callback to handle the '--reset' flag.
+    def reset_offset(self, consumer, partitions):
+        for p in partitions:
+            p.offset = OFFSET_END
+        consumer.assign(partitions)
 
     def consommer_messages(self):
         if self.consommateur is None:
