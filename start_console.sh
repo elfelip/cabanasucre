9934f8b4ee98ee6d6ae963bec70ba0fb0d4c1429
Philippe Gauthier <elfelip@yahoo.com>
2023-03-11 10:27:54 -0500

    Automatiser le démarrage des composants

--- start_console.sh
+++ start_console.sh
@@ -1,4 +1,4 @@
-echo Attendre 30 secondes avant de demarrer Cabanasucre
+echo Attendre 30 secondes avant de demarrer la console Cabanasucre
 sleep 30
 echo Demarrage Console Cabanasucre
 python3 /home/pi/cabanasucre/bouillage_ctrl_cmd/console_sucrier.py
