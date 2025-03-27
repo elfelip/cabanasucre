7d1a36fcbf99a2d4187897463d2caf7fd3d0914f
Philippe Gauthier <elfelip@yahoo.com>
2025-03-27 17:58:20 -0400

    Ajuster le niveau bas a 3 et niveau haut a 4

--- start_cabanasucre.sh
+++ start_cabanasucre.sh
@@ -1,4 +1,4 @@
 echo Attendre 30 secondes avant de demarrer Cabanasucre
 sleep 30
 echo Demarrage Cabanasucre
-python3 /home/pi/cabanasucre/bouillage_ctrl_cmd/bouillage_controle.py
+python3 /home/pi/cabanasucre/bouillage_ctrl_cmd/bouillage_controle.py --niveaubas=3 --niveauhaut=4
